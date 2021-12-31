5626baa26a872e784cf3325513b93eb7b298819c
jhonatanAlanFerreira <jhonatan.alanf@gmail.com>
2021-12-30 21:21:17 -0300

    Testes da barra lateral

--- frontend/potential-crud/src/app/components/menu-options/menu-options.component.spec.ts
+++ frontend/potential-crud/src/app/components/menu-options/menu-options.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { MenuOptionsComponent } from './menu-options.component';
 
@@ -9,6 +11,10 @@ describe('MenuOptionsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule.withRoutes([
+        { path: 'levels', component: DummyComponent },
+        { path: 'developers', component: DummyComponent }
+      ])],
       declarations: [MenuOptionsComponent]
     })
       .compileComponents();
@@ -36,4 +42,41 @@ describe('MenuOptionsComponent', () => {
     expect(pElements[1].nativeElement.textContent).toBe('Níveis');
   });
 
+  it('should toggle class hidden', () => {
+    const menuBar = fixture.debugElement.query(By.css('.menu-options'));
+    const menuBarElement = menuBar.nativeElement;
+    expect(menuBarElement).toHaveClass('hidden');
+    component.service.sideBarOpen = true;
+    fixture.detectChanges();
+    expect(menuBarElement).not.toHaveClass('hidden');
+  });
+
+  it('should go to levels', () => {
+    const router = TestBed.inject(Router);
+    spyOn(router, 'navigateByUrl');
+
+    const levelsLink = fixture.debugElement.query(
+      debugEl => debugEl.name === 'p' && debugEl.nativeElement.textContent === 'Níveis'
+    ).nativeElement;
+
+    levelsLink.click();
+
+    expect(router.navigateByUrl).toHaveBeenCalledWith(router.createUrlTree(['levels']), { skipLocationChange: false, replaceUrl: false, state: undefined });
+  });
+
+  it('should go to developers', () => {
+    const router = TestBed.inject(Router);
+    spyOn(router, 'navigateByUrl');
+
+    const levelsLink = fixture.debugElement.query(
+      debugEl => debugEl.name === 'p' && debugEl.nativeElement.textContent === 'Desenvolvedores'
+    ).nativeElement;
+
+    levelsLink.click();
+
+    expect(router.navigateByUrl).toHaveBeenCalledWith(router.createUrlTree(['developers']), { skipLocationChange: false, replaceUrl: false, state: undefined });
+  });
+
 });
+
+class DummyComponent { }
