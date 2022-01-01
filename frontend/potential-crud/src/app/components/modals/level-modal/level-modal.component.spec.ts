d2749c7c0020b14c4e7b2ca201f4b3a1d4cfa837
jhonatanAlanFerreira <jhonatan.alanf@gmail.com>
2022-01-01 14:32:53 -0300

    Teste da modal de níveis

--- frontend/potential-crud/src/app/components/modals/level-modal/level-modal.component.spec.ts
+++ frontend/potential-crud/src/app/components/modals/level-modal/level-modal.component.spec.ts
@@ -1,6 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { ILevel } from 'src/app/entities/ILevel';
 import { ModalComponent } from '../modal/modal.component';
 
 import { LevelModalComponent } from './level-modal.component';
@@ -26,5 +28,58 @@ describe('LevelModalComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+
+  it('should have a label', () => {
+    const labels = fixture.debugElement.queryAll(By.css('label'));
+    expect(labels.length).toBe(1);
+    expect(labels[0].nativeElement.textContent).toBe('*Nível');
+  });
+
+  it('should have a input', () => {
+    const inputs = fixture.debugElement.queryAll(By.css('input'));
+    expect(inputs.length).toBe(1);
+  });
+
+  it('should be invalid', () => {
+    const form = component.form;
+    expect(form.valid).toBeFalsy();
+  });
+
+  it('should be valid', () => {
+    let developer: ILevel = {
+      nivel: '61c57f5fba1f11631503c33c',
+      qtd: 1
+    };
+    component.setValue(developer);
+
+    fixture.detectChanges();
+    expect(component.form.valid).toBeTruthy();
+  });
+
+  it('should not submit invalid form', () => {
+    const submitButton = fixture.debugElement.query(By.css('#submit-button'));
+    spyOn(component.save, 'emit');
+    submitButton.nativeElement.click();
+    fixture.detectChanges();
+    expect(component.save.emit).not.toHaveBeenCalled();
+  });
+
+  it('should submit event', () => {
+    const submitButton = fixture.debugElement.query(By.css('#submit-button'));
+    spyOn(component.save, 'emit');
+
+    let developer: ILevel = {
+      nivel: '61c57f5fba1f11631503c33c',
+      qtd: 1
+    };
+    
+    component.setValue(developer);
+
+    fixture.detectChanges();
+    submitButton.nativeElement.click();
+    fixture.detectChanges();
+    expect(component.save.emit).toHaveBeenCalled();
   });
 });
