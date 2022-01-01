8585d5af28d52b5e62805f90f4d5fdd96c5fe2e7
jhonatanAlanFerreira <jhonatan.alanf@gmail.com>
2022-01-01 14:40:50 -0300

    Removido linha não utilizada

--- frontend/potential-crud/src/app/app.component.spec.ts
+++ frontend/potential-crud/src/app/app.component.spec.ts
@@ -20,9 +20,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'potential-crud'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-  });
-
 });
