03e2da3d5ff502d16157b9080356b90bf3123a7f
jhonatanAlanFerreira <jhonatan.alanf@gmail.com>
2022-01-01 14:49:06 -0300

    Teste do pipe de gênero

--- frontend/potential-crud/src/app/pipes/genre.pipe.spec.ts
+++ frontend/potential-crud/src/app/pipes/genre.pipe.spec.ts
@@ -1,8 +1,16 @@
 import { GenrePipe } from './genre.pipe';
 
 describe('GenrePipe', () => {
+  const pipe = new GenrePipe();
+
   it('create an instance', () => {
-    const pipe = new GenrePipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should replace to M or F', () => {
+    expect(pipe.transform('M')).toBe('Masculino');
+    expect(pipe.transform('F')).toBe('Feminino');
+    expect(pipe.transform('A')).toBe('A');
+  });
+
 });
